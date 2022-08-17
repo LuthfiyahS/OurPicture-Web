b20b2355da99879ef57281e8e63f8960586eef9b
Raihan Rafid <raihanrfd1@gmail.com>
2022-08-17 19:58:57 +0700

    features/starter-project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import NextNProgress from "nextjs-progressbar";
+import { ReactNotifications } from "react-notifications-component";
+import "react-notifications-component/dist/theme.css";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [showChild, setShowChild] = useState(false);
+
+  useEffect(() => setShowChild(true), []);
+
+  if (!showChild) return null;
+
+  if (typeof window === "undefined") return <></>;
+  else
+    return <Provider store={store}>
+      <NextNProgress
+        color="#E30C1A"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+      <ReactNotifications />
+      <Component {...pageProps} />
+    </Provider>
 }
 
 export default MyApp
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#FB8500",
+        },
+        secondary: {
+          DEFAULT: "#CACACAs"
+        }
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+}
+
+
